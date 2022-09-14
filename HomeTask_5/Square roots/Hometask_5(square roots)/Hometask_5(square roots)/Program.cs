201cb48f4121aaf161b85f9c1ecd347ce114f960
Dmitriy Pimenov <111114932+bullydude@users.noreply.github.com>
2022-09-14 18:31:35 +0300

    Hometask_5

---
+++ HomeTask_5/Square roots/Hometask_5(square roots)/Hometask_5(square roots)/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace Hometask_5_square_roots_
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter first number: ");
+            int FirstArg = int.Parse(Console.ReadLine());
+            Console.Write("Please enter second number: ");
+            int SecondArg = int.Parse(Console.ReadLine());
+            var startNumber = Math.Min(FirstArg, SecondArg);
+            var finishNumber = Math.Max(FirstArg, SecondArg);
+            var count = finishNumber - startNumber + 1;
+            var range = Enumerable.Range(startNumber, count);
+            Parallel.ForEach(range, SquareRootsCalc);
+            
+        }
+
+        public static void SquareRootsCalc(int a)
+        {
+            Console.WriteLine($"Square root for {a} is {Math.Sqrt(a)}.");
+        }
+    }
+}
---
+++ HomeTask_5/Сoncatenate the content/Hometask_5 (Сoncatenate the content)/Hometask_5 (Сoncatenate the content)/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+namespace Hometask_5_Сoncatenate_the_content
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Enter files directory: ");
+            string fileDirectory = Console.ReadLine() ?? string.Empty;
+
+            var txtFolder = Directory.EnumerateFiles(fileDirectory, "*.txt");
+            var strBuilder = new StringBuilder();
+            foreach (var txtPath in txtFolder)
+            {
+                var text = File.ReadAllTextAsync(txtPath, CancellationToken.None).Result;
+                strBuilder.Append(text + "\n");
+            }
+            File.WriteAllTextAsync($"{fileDirectory}//Result.txt",
+                strBuilder.ToString(),
+                Encoding.Unicode,
+                CancellationToken.None);
+
+        }
+    }
+}
